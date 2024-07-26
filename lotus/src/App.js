c6566fd1a410579c9de768b5f4fac7f46206522f
kawadeajinkyak1 <ajinkyak192@gmail.com>
2024-07-26 15:57:58 +0530

    hashrouteradded

--- lotus/src/App.js
+++ lotus/src/App.js
@@ -1,30 +1,64 @@
-
 import './App.css';
 import NavBar from './components/NavBar';
-import { BrowserRouter, Route,Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import About  from './components/About';
+import About from './components/About';
 import Contact from './components/Contact';
 import Booking from './components/Booking';
 import Suite from './components/Suite';
 import Gallery from './components/Gallery';
 import Mini from './components/Mini';
 import Excutive from './components/Excutive';
+
 function App() {
   return (
     <>
-    <BrowserRouter>
-  
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />}/>
-      <Route path="/booking" element={<Booking />} />
-      <Route path="/contact" element={<Contact />}/>
-      <Route path="/gallery" element={<Gallery />}/>
-    </Routes>
-    </BrowserRouter>
+      <HashRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/suite" element={<Suite />} />
+          <Route path="/mini" element={<Mini />} />
+          <Route path="/executive" element={<Excutive />} />
+        </Routes>
+      </HashRouter>
     </>
   );
 }
 
 export default App;
+
+
+// import './App.css';
+// import NavBar from './components/NavBar';
+// import { BrowserRouter, Route,Routes } from 'react-router-dom';
+// import Home from './components/Home';
+// import About  from './components/About';
+// import Contact from './components/Contact';
+// import Booking from './components/Booking';
+// import Suite from './components/Suite';
+// import Gallery from './components/Gallery';
+// import Mini from './components/Mini';
+// import Excutive from './components/Excutive';
+// function App() {
+//   return (
+//     <>
+//     <BrowserRouter>
+  
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/about" element={<About />}/>
+//       <Route path="/booking" element={<Booking />} />
+//       <Route path="/contact" element={<Contact />}/>
+//       <Route path="/gallery" element={<Gallery />}/>
+//     </Routes>
+//     </BrowserRouter>
+//     </>
+//   );
+// }
+
+// export default App;
